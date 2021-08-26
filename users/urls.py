a4fae25956dfaea593f62967624a0c6678118119
Hanlin Miao <hanlinmiao@Hanlins-MBP.home>
2021-08-26 11:53:49 -0400

    made some changes with user-stock-dashboard

--- users/urls.py
+++ users/urls.py
@@ -5,6 +5,7 @@ from .views import (
     profile,
     user_stock_dashboard,
     add_stock_to_watchlist,
+    remove_stock_from_watchlist,
 )
 
 urlpatterns = [
@@ -25,5 +26,6 @@ urlpatterns = [
          auth_views.PasswordResetCompleteView.as_view(template_name = 'users/password_reset_complete.html'), 
          name ='password_reset_complete'),
      path('<str:username>/user-stock-dashboard/', user_stock_dashboard, name='user-stock-dashboard'),
-     path('<str:username>/<str:stock>/add-stock', add_stock_to_watchlist, name='user-add-stock')
+     path('<str:username>/<str:stock>/add-stock', add_stock_to_watchlist, name='user-add-stock'),
+     path('<str:username>/<str:stock>/remove-stock', remove_stock_from_watchlist, name='user-remove-stock')
 ]
--- users/views.py
+++ users/views.py
@@ -47,7 +47,7 @@ def user_stock_dashboard(request, username):
 	stocks = profile.get_stocks().split(",")
 	context = {
 		"user": user,
-		"stocks": stocks,
+		"stocks": sorted(stocks),
 	}
 
 	return render(request, 'users/track_stocks.html', context)
@@ -58,20 +58,40 @@ def add_stock_to_watchlist(request, username, stock):
 	profile = Profile.objects.get(user=user)
 	watch_list = profile.stocks.split(",")
 	if stock in watch_list:
-		messages.warning(request, f'This stock {stock} is already in your watchlist')
+		messages.warning(request, f'{stock} is already in your watchlist')
 		return redirect('index')
 	else:
 		profile.stocks += ("," + stock)
 		profile.save()
 		watch_list.append(stock)
-		messages.success(request, f'This stock {stock} has been added to your watchlist')
+		messages.success(request, f'{stock} has been added to your watchlist')
 	
 	context = {
 		"user": user,
 		"stocks": sorted(watch_list),
 	}
 
-	return render(request, 'users/track_stocks.html', context)
+	return redirect('user-stock-dashboard', user)
+
+def remove_stock_from_watchlist(request, username, stock):
+	user = User.objects.get(username=username)
+	profile = Profile.objects.get(user=user)
+	watch_list = profile.stocks.split(",")
+	if stock not in watch_list:
+		messages.warning(request, f'This stock {stock} is not in your watchlist')
+		return redirect('index')
+	else:
+		watch_list.remove(stock)
+		profile.stocks = ",".join(watch_list)
+		profile.save()
+		messages.success(request, f'{stock} has been removed from your watchlist')
+	
+	context = {
+		"user": user,
+		"stocks": sorted(watch_list),
+	}
+
+	return redirect('user-stock-dashboard', user)
 
 	
 	
